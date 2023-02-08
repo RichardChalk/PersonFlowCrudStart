dbb24b618c0d5e02b939c5beb94e8719abc7e35d
Richard Chalk <barmybrit@gmail.com>
2023-02-08 07:08:52 +0100

    2022-02-08 3

--- PersonFlowCrudStart/Pages/Person/New.cshtml.cs
+++ PersonFlowCrudStart/Pages/Person/New.cshtml.cs
@@ -7,12 +7,37 @@ using SkysFormsDemo.Services;
 
 namespace SkysFormsDemo.Pages.Person
 {
+    [BindProperties]
     public class NewModel : PageModel
     {
+        [MaxLength(100)]
+        [Required]
+        public string Name { get; set; }
+
+        [StringLength(100)]
+        public string StreetAddress { get; set; }
+
+        [StringLength(10)]
+        public string PostalCode { get; set; }
+
+        [StringLength(2)]
+        public string CountryCode { get; set; }
+
+        [Range(0, 100000, ErrorMessage = "Skriv ett tal mellan 0 och 100000")]
+        public decimal Salary { get; set; }
+
+        [Range(0, 100)]
+        public int Age { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        public string City { get; set; }
+
+        [StringLength(150)]
+        [EmailAddress]
+        public string Email { get; set; }
         public void OnGet()
         {
-
         }
-
     }
 }
